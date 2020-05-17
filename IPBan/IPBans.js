0e930fb39781d4d7d9b0d9a68ffba7684d161151
SacriPudding <62363010+SacriPudding@users.noreply.github.com>
2020-05-17 14:38:42 -0500

    Update IPBans.js

--- IPBan/IPBans.js
+++ IPBan/IPBans.js
@@ -1,35 +1,3 @@
-/*
-    This is the IP Banning script. It essentially works by creating a constant log of players IPs as well as controls
-    the command for banning players.
-
-    This script relies on another, simpler, script. permissionHandler.js. Its used for making sure only someone
-    has operator perms. It also relies on an index
-
-    In order to use this script, you must be an op and be on the server. Due to limitations, I can't read console
-    so you have to be in game.
-
-    For IP banning, the player you are banning DOES NOT need to be online. Banning will work whether or not said
-    person is online.
-
-    One downside to this script is that it is not possible to ban new IPs the user joins on after they are on
-    the blacklist. They won't be able to get on as they are still server banned but if they make a new account
-    they will be able to join again.
-
-    Known bugs:
-        None! Go ahead and send me a message
-            Social medias:
-                Twitter: @SacriPudding
-                Discord: @SacriPudding#1281
-                Telegram: @SacriPudding
-                Reddit: u/evan13lee
-                GitHub: Take a guess
-    Plans:
-        VPN detection VIA GeoIP
-        Console based command. (Need to find some way to talk to console. Currently in development.)
-
-    PS: A lot of this came from my friend Luke. This was my first "real" plugin and also one of my first JS scripts
-    in general. Follow him over at @ConsoleLogLuke. He is into mostly iPhone jailbreaking.
- */
 // Exports this function so its accessible in index.js. If you want to use this standalone (I don't advise it.)
 // Import here instead.
 export function runScriptIpBans(system, onPlayerJoined, onChat, sendBroadcast, executeCommand,getPlayerByNAME, runScriptPermissionHandler, addByNAME, addByXUID, open, getOfflinePlayerByNAME) {
